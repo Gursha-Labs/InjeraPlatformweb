d3beb195728eb809ad61c8158829c5957d779907
naol <naolmeseret22@gmail.com>
2025-11-03 19:41:35 +0300

    adding a ui for login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,26 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./pages/user/Home"
 import RootLayout from "./layout/RootLayout"
 import { ThemeProvider } from "./components/theme-provider"
+import { Toaster } from "./components/ui/sonner"
+import Profile from "./pages/user/Profile"
+import Login from "./pages/user/Login"
+import Signup from "./pages/Signup"
+import ProfileBuild from "./pages/user/ProfileBuild"
 
 
 function App() {
   const router = createBrowserRouter(
     [
+      {
+        path: "/login", element: <Login />
+      },
+      {
+        path: "/signup", element: <Signup />
+      },
+      {
+        path: "/user-profile-build", element: <ProfileBuild />
+      },
+
       {
         element: <RootLayout />,
         children: [
@@ -21,13 +36,18 @@ function App() {
           },
           {
             path: "/setting", element: <Home />
-          }
+          },
+          {
+            path: "/profile", element: <Profile />
+          },
+
         ]
       }
     ])
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <RouterProvider router={router} />
+      <Toaster position="top-center" expand={true} richColors />
     </ThemeProvider>
   )
 
