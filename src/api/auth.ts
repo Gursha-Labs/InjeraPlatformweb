e988ae7c7256f44c963c60bdea4d951432182897
naol <naolmeseret22@gmail.com>
2025-11-10 15:29:26 +0300

    make a global error handler for apicall

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,118 +3,54 @@ import type {
   LoginResponse,
   RegistrationRequest,
   RegistrationResponse,
+  ResetPasswordParam,
   VarifyEmailRequest,
   VarifyEmailResponse,
 } from "@/types/api/auth";
 import apiClient from "./apiClinet";
+import { handleApiResponse } from "@/lib/handleApiResponse";
 
 export const registerUser = async ({
   username,
   password,
   email,
   type,
-}: RegistrationRequest): Promise<RegistrationResponse> => {
-  try {
-    const res = await apiClient.post("/register", {
+}: RegistrationRequest) => {
+  return handleApiResponse<RegistrationResponse>(() =>
+    apiClient.post("/register", {
       email,
       username,
       password,
       type,
-    });
-
-    const data = res.data;
-    if (!data) throw new Error("error registration the user");
-    return {
-      message: data.message,
-      user: data.user,
-      requires_verification: data.requires_verification,
-    };
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
-
-    throw new Error(String(err));
-  }
+    })
+  );
 };
 
-export const varifyEmail = async ({
-  email,
-  otp,
-}: VarifyEmailRequest): Promise<VarifyEmailResponse> => {
-  try {
-    const res = await apiClient.post("/verify-email", {
+export const varifyEmail = async ({ email, otp }: VarifyEmailRequest) => {
+  return handleApiResponse<VarifyEmailResponse>(() =>
+    apiClient.post("/verify-email", {
       email,
       otp,
-    });
-    const data = res.data;
-    if (!data) throw new Error("please enter the correct otp ");
-
-    return {
-      message: data.message,
-      user: data.user,
-      token: data.token,
-    };
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
-    throw new Error(String(err));
-  }
+    })
+  );
 };
 
-export const login = async ({
-  login,
-  password,
-}: LoginRequest): Promise<LoginResponse> => {
-  try {
-    const res = await apiClient.post("/login", {
+export const login = async ({ login, password }: LoginRequest) => {
+  return handleApiResponse<LoginResponse>(() =>
+    apiClient.post("/login", {
       login,
       password,
-    });
-
-    if (res.status === 403) throw new Error("Please verify your email");
-
-    const data = res.data;
-
-    if (!data) throw new Error("Invalid Creadental ");
-
-    return {
-      user: data.user,
-      message: data.message,
-      token: data.token,
-    };
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
-    throw new Error(String(err));
-  }
+    })
+  );
 };
 
 export const forgotPassword = async ({ email }: { email: string }) => {
-  try {
-    const res = await apiClient.post("/forgot-password", {
+  return handleApiResponse(() =>
+    apiClient.post("/forgot-password", {
       email,
-    });
-    const data = res.data;
-    if (!data) throw new Error("please enter valid email");
-    return {
-      message: data.message,
-    };
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
-    throw new Error(String(err));
-  }
+    })
+  );
 };
-interface ResetPasswordParam {
-  password: string;
-  conformpassword: string;
-  otp: string;
-  email: string;
-}
 
 export const resetPassword = async ({
   password,
@@ -122,24 +58,12 @@ export const resetPassword = async ({
   otp,
   email,
 }: ResetPasswordParam) => {
-  try {
-    const res = await apiClient.post("/reset-password", {
+  return handleApiResponse(() =>
+    apiClient.post<ResetPasswordParam>("/reset-password", {
       email,
       password,
       password_confirmation: conformpassword,
       otp,
-    });
-    const data = res.data;
-
-    if (!data) throw new Error("something went Wrong");
-
-    return {
-      message: data.message,
-    };
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      throw new Error(err.message);
-    }
-    throw new Error(String(err));
-  }
+    })
+  );
 };
